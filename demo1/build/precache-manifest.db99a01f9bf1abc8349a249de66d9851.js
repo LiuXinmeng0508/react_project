c3a0653b7a805b66c4ab203473ec998a98a41c94
LiuXinmeng <1013430695@qq.com>
2019-10-26 20:43:17 +0800

    Add files via upload

---
+++ demo1/build/precache-manifest.db99a01f9bf1abc8349a249de66d9851.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c36eebe25c35c23c010967ea5682699",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df2fec01e543cab9a896",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2ff70468faf272def4f3",
+    "url": "/static/js/2.3a811fe3.chunk.js"
+  },
+  {
+    "revision": "df2fec01e543cab9a896",
+    "url": "/static/js/main.c2ddf3e8.chunk.js"
+  },
+  {
+    "revision": "f8217d9ec28937a0e7bd",
+    "url": "/static/js/runtime-main.53d7e9f2.js"
+  }
+]);
