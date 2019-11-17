080878847cb5df6155102030efd1bfac8c7b1a53
LiuXinmeng <1013430695@qq.com>
2019-11-17 19:41:02 +0800

    美食作业up

---
+++ demo3/build/precache-manifest.b49fb94aafd0c4a383819423086917c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc761e7d825fa2844c32eba74ede9d41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "928a7fe3b121fe3e98da",
+    "url": "/static/css/2.6f60e7f3.chunk.css"
+  },
+  {
+    "revision": "e7c19508039d828d7505",
+    "url": "/static/css/main.e21b730d.chunk.css"
+  },
+  {
+    "revision": "928a7fe3b121fe3e98da",
+    "url": "/static/js/2.c1806dbe.chunk.js"
+  },
+  {
+    "revision": "e7c19508039d828d7505",
+    "url": "/static/js/main.06dee56c.chunk.js"
+  },
+  {
+    "revision": "de98738af81fd1e11c74",
+    "url": "/static/js/runtime-main.866e7cb5.js"
+  }
+]);
